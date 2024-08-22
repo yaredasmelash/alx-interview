603a253d24cd05036ebdd7dc27b1b26a9c0e6574
Yared Asmelash <yaredasmelash00@gmail.com>
2024-08-22 21:01:44 +0300

    Rewrite Validate

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,40 +1,36 @@
 #!/usr/bin/python3
-"""Determines a valid UTF-8 encoding"""
+"""UTF-8 Validation Module
+"""
 
 
 def validUTF8(data):
-    """
-    bit1 checks if significant byte is 1
-    bit2 checks if second significant byte is 0
-    nbytes keeps track of how many 1s before 0 occurs
-    data represented by a list of integers to check
-    """
+    """Determines if a given data set represents a valid UTF-8 encoding.
 
-    bit1 = 1 << 7
-    bit2 = 1 << 6
-    nbytes = 0
+    Args:
+        data (list): list of integers
 
-    if not data or len(data) == 0:
-        return True
+    Returns:
+        bool: True if data is a valid UTF-8 encoding, else return False
+    """
+    n_bytes = 0
 
     for num in data:
-        bit = 1 << 7
-        if nbytes == 0:
-            while (bit & num):
-                nbytes += 1
-                bit = bit >> 1
-
-            if nbytes == 0:
+        byte = num & 0xff
+        if n_bytes == 0:
+            mask1 = 1 << 7
+            mask2 = 1 << 6
+            while mask1 & byte:
+                n_bytes += 1
+                mask1 = mask1 >> 1
+                mask2 = mask2 >> 1
+            if n_bytes == 0:
                 continue
-            if nbytes == 1 or nbytes > 4:
+            if n_bytes == 1 or n_bytes > 4:
                 return False
         else:
-
-            if not (num & bit1 and not (num & bit2)):
+            mask1 = 1 << 7
+            mask2 = 1 << 6
+            if not (byte & mask1 and not (byte & mask2)):
                 return False
-        nbytes -= 1
-
-    if nbytes:
-        return False
-    else:
-        return True
+        n_bytes -= 1
+    return n_bytes == 0
