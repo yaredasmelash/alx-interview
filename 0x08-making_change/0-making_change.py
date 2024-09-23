1b716ce53eafe33b478adabae4864afafd44402f
Yared Asmelash <yaredasmelash00@gmail.com>
2024-09-24 01:20:30 +0300

    Inreview

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,25 +1,33 @@
 #!/usr/bin/python3
-"""Change making module.
+"""
+Task: Change comes from within
+Given a pile of coins of different values,
+determine the fewest number of coins needed to
+meet a given amount total
 """
 
 
 def makeChange(coins, total):
-    """Determines the fewest number of coins needed to meet a given
-    amount total when given a pile of coins of different values.
     """
+    Given a pile of coins of different values, determine the fewest
+    number of coins needed to meet a given amount total.
+    Return: fewest number of coins needed to meet total
+        - If total is 0 or less, return 0
+        - If total cannot be met by any number of coins you have, return -1
+        - Coins is a list of the values of the coins in your possession
+        - The value of a coin will always be an integer greater than 0
+        - You can assume you have an infinite number of each denomination of
+        coin in the list
+    """
+    n_coins = 0
     if total <= 0:
-        return 0
-    rem = total
-    coins_count = 0
-    coin_idx = 0
-    sorted_coins = sorted(coins, reverse=True)
-    n = len(coins)
-    while rem > 0:
-        if coin_idx >= n:
-            return -1
-        if rem - sorted_coins[coin_idx] >= 0:
-            rem -= sorted_coins[coin_idx]
-            coins_count += 1
-        else:
-            coin_idx += 1
-    return coins_count
+        return n_coins
+    coins.sort(reverse=True)
+    while (total > 0 and coins):
+        n = int(total / coins[0])
+        total = total - (coins[0] * n)
+        n_coins = n_coins + n
+        coins.remove(coins[0])
+    if total != 0:
+        return -1
+    return n_coins
